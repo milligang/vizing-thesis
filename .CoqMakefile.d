4e20fbcbdf59214ddb68e0ef3fc28fcab984cfb3
milligang <milligan.grinstead@gmail.com>
2026-03-03 15:47:00 -0500

    separated vizings theorem from edge coloring file

--- .CoqMakefile.d
+++ .CoqMakefile.d
@@ -6,5 +6,7 @@ theories/edge_coloring.vo theories/edge_coloring.glob theories/edge_coloring.v.b
 theories/edge_coloring.vos theories/edge_coloring.vok theories/edge_coloring.required_vos: theories/edge_coloring.v theories/aux.vos /Users/milligan/.opam/coq-8.19/lib/rocq-runtime/rocqworker
 theories/tmp.vo theories/tmp.glob theories/tmp.v.beautified theories/tmp.required_vo: theories/tmp.v /Users/milligan/.opam/coq-8.19/lib/rocq-runtime/rocqworker
 theories/tmp.vos theories/tmp.vok theories/tmp.required_vos: theories/tmp.v /Users/milligan/.opam/coq-8.19/lib/rocq-runtime/rocqworker
+theories/vizing.vo theories/vizing.glob theories/vizing.v.beautified theories/vizing.required_vo: theories/vizing.v theories/aux.vo theories/edge_coloring.vo /Users/milligan/.opam/coq-8.19/lib/rocq-runtime/rocqworker
+theories/vizing.vos theories/vizing.vok theories/vizing.required_vos: theories/vizing.v theories/aux.vos theories/edge_coloring.vos /Users/milligan/.opam/coq-8.19/lib/rocq-runtime/rocqworker
 depend-graph/depend_build.vo depend-graph/depend_build.glob depend-graph/depend_build.v.beautified depend-graph/depend_build.required_vo: depend-graph/depend_build.v theories/edge_coloring.vo /Users/milligan/.opam/coq-8.19/lib/rocq-runtime/rocqworker
 depend-graph/depend_build.vos depend-graph/depend_build.vok depend-graph/depend_build.required_vos: depend-graph/depend_build.v theories/edge_coloring.vos /Users/milligan/.opam/coq-8.19/lib/rocq-runtime/rocqworker
